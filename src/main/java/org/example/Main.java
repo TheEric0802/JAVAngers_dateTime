7c70525aa879ac4056e98190ea95028b746561cb
Eric Schmidt <eric0802@web.de>
2025-04-13 13:32:27 +0200

    output current date + 2 weeks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
+        System.out.println(LocalDate.now().plusWeeks(2));
     }
 }
