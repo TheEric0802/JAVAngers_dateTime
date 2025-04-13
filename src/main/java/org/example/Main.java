0808645e7ef58eb51595a128f892d531e9d34dbe
Eric Schmidt <eric0802@web.de>
2025-04-13 13:38:02 +0200

    add date comparison

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
         System.out.println(LocalDate.now().plusWeeks(2));
+
+        LocalDate date = LocalDate.of(2026, 1, 1);
+        int result = LocalDate.now().compareTo(date);
+        if (result == 0) {
+            System.out.println("Same");
+        } else if (result > 0) {
+            System.out.println("Later");
+        } else {
+            System.out.println("Earlier");
+        }
     }
 }
